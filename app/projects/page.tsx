3ce9d2f80b53109c5e7d648908ea9c4fe2163a29
Halit Alptekin <1284666+halit@users.noreply.github.com>
2026-06-11 13:51:23 +0000

    Update the description of projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ import { createCollectionPage } from '@/lib/page-logic';
 
 const { metadata, Page } = createCollectionPage({
   title: 'Projects',
-  description: 'A collection of security tools, libraries, and experiments.',
+  description: 'A collection of tools, libraries, and experiments.',
   url: '/projects/',
   type: 'project',
 });
